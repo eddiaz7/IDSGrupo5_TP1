1d22015caccbe6bf5852901f35126db7eaa5bffd
Rodrigo41 <rodrigomegag@hotmail.com>
2021-09-24 19:40:38 -0300

    primera publicacion

---
+++ src/Hola.java
@@ -0,0 +1,17 @@
+
+public class Hola {
+	private String saludo;
+	public String getSaludo() {
+		return saludo;
+	}
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	public static void main(String[] args) {
+		Hola hola = new Hola();
+		hola.setSaludo("Prueba");
+		System.out.println(hola.getSaludo());
+       
+	}
+
+}
